8aeac652b2c28cfeffda48f7770d459b75e445e8
JulianEcheverri <julianecheverri@outlook.com>
2020-08-21 15:43:08 -0500

    Llamando api youtube

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { YoutubeService } from '../../services/youtube.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _youtubeService: YoutubeService) { }
 
   ngOnInit(): void {
+    // El subscribe es necesario por que se debe de estar pendiente de la respuesta de la peticion y los datos que retorna
+    this._youtubeService.getVideos().subscribe(resp => {
+      console.log(resp);
+    });
+
   }
 
 }
--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
+  private readonly _youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private readonly _apiKey = 'AIzaSyDeP6pYtVtzvGWDhy7dFPETOMm1UAs9Ivc';
+  private _playListId = 'UUuaPTYj15JSkETGnEseaFFg';
+  private _nextPageToken = '';
 
-  constructor() { }
+
+  constructor(private _httpClient: HttpClient) {
+  }
+
+  getVideos() {
+    const url = `${this._youtubeUrl}/playlistItems`;
+    const params = new HttpParams()
+      .set('part', 'snippet')
+      .set('maxResults', '10')
+      .set('playlistId', this._playListId)
+      .set('key', this._apiKey)
+
+    return this._httpClient.get(url, { params });
+  }
 }
